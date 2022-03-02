45c444e19aac37b443b86cad5d4e5917e2104e75
mattanmohel <mattanmohel@gmail.com>
2022-03-01 23:02:41 -0500

    More Progress Towards Functionality

--- src/lex/parser.rs
+++ src/lex/parser.rs
@@ -2,9 +2,53 @@
 use crate::core::objects::Node;
 use super::tokens::{Tok, Spec};
 
+
+
+/*
+
+parses code into recursive linked lists of Objects
+
+________________________
+
+1: (set x (+ 5 5))
+2: (+= x 10)   
+3: 
+4: x 
+________________________
+
+translates to...
+________________________
+
+(...) --> (...) --> 'x'
+  \         \__ '+=' --> 'x' --> '10'
+   \ 
+    \__ 'set' --> 'x' --> (...)
+                            \__ '+' --> '5' --> '5'
+________________________
+
+this form can be easily traversed and evaluated
+
+*/
+
 pub fn tokens_to_nodes(tokens: &Vec<Tok>) -> *mut Node {
-    for spec in tokens.iter().map(|tok| &tok.spec) {
-        
+    for tok in tokens.iter() {
+        match &tok.spec {
+            Spec::ListBeg => {
+                
+            },
+
+            Spec::ListEnd => {
+
+            },
+
+            Spec::Symbol => {
+
+            },
+
+            Spec::Value(val) => {
+
+            },
+        }
     }
 
     todo!()
